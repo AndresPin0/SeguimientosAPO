517ae920e4afa0eea973c609607d2169dbd860ad
AndresPin0 <84475698+AndresPin0@users.noreply.github.com>
2022-03-25 07:53:21 -0500

    ExactSum - First Commit
    Exercise accepted by OnlineJudge

---
+++ ExactSum-11057/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main( String[] args ) {
+        
+        Main m = new Main();
+        m.begin();
+        
+    }
+    
+    void begin(){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int N = Integer.parseInt( sc.next() );
+            int[] a = new int[N];
+            for(int i=0; i<N; ++i){
+               a[i] = Integer.parseInt( sc.next() ); 
+            }
+            int money = Integer.parseInt(sc.next());
+            int book1=-1 , book2=-1;
+            for(int i=0; i<N-1; ++i){
+                
+                for(int j=i+1; j<N; ++j){
+                    
+                    if( a[i]+a[j] == money ){
+                        if( book1 == -1 && book2 == -1 ){
+                            book1=i;
+                            book2=j;
+                        }
+                        else if( Math.abs(a[i]-a[j])<Math.abs(a[book1]-a[book2]) ){
+                            book1=i;
+                            book2=j;
+                        }
+                    }
+                }
+            }
+            int i=a[book1] , j=a[book2] ;
+            System.out.printf("Peter should buy books whose prices are %d and %d.\n\n", Math.min(i,j) , Math.max(i,j)  );
+        }
+    }
+}
